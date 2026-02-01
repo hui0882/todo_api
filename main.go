efa300630d698007d1780ccc2070bbfc3fd69bc2
hui0882 <xiao0882@qq.com>
2026-02-01 21:49:39 +0800

    增加了 swagger 支持

--- main.go
+++ main.go
@@ -1,1 +1,19 @@
 package main
+
+import (
+	"log"
+	_ "todo-api/docs"
+	"todo-api/router"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	// 检查是否成功加载了 docs 包（通过编译期检查）
+	log.Println("✅ Server starting...")
+
+	gin.SetMode(gin.DebugMode)
+	r := router.SetupRouter()
+	r.Use(gin.Recovery())
+	r.Run(":8080")
+}
--- router/router.go
+++ router/router.go
@@ -1,1 +1,19 @@
 package router
+
+import (
+	"todo-api/controller"
+
+	"github.com/gin-gonic/gin"
+	swaggerFiles "github.com/swaggo/files"
+	ginSwagger "github.com/swaggo/gin-swagger"
+)
+
+func SetupRouter() *gin.Engine {
+	r := gin.Default()
+
+	testGroup := r.Group("/test")
+	testGroup.GET("/test", controller.Test)
+
+	r.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
+	return r
+}
